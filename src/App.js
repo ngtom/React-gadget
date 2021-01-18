314469c6bf07818470c761025817a8de3260537a
Thomas <slash_tom@yahoo.com>
2021-01-18 22:06:37 +0100

    desktop gadget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+// to make the website responsive, we need to use react hook: useState
+// react component:javascript function to render something
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Folder name="Desktop">
+        <File name="Kitty.jpeg"/>
+        <File name="Meow.png" />
+        <Folder name="Application">      
+          <File name="blah-blah.mp4"/>
+          <File name="its_my_life.mp4"/>
+        </Folder>
+      </Folder>
+      <Folder name="Music">
+        <File name="bohemian_rhapsody.mp4"/>
+        <File name="presentation.pdf"/>
+        <File name="thesis.pdf"/>
+      </Folder>
     </div>
   );
 }
 
+// another react component
+// attribute : props 
+const Folder = (props) => {
+  const [isOpen, setIsOpen] = useState(true)
+  const {name, children} = props;
+  
+  const direction = isOpen ? "down" : "right"
+  const handleClick = () =>{
+    setIsOpen(! isOpen)
+  }
+  return (
+    <div style={{marginLeft: '20px'}}>
+      <span onClick={handleClick}>
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>
+      {name}
+      <div style={{marginLeft: '17px'}}>
+        {isOpen? children : null}
+      </div>
+    </div>
+  );
+}
+
+const File = (props) =>{
+  const {name} = props;
+  const extension = name.split(".")[1];
+  const fileIcons = {
+    mp4: 'headphones',
+    png: 'file image outline',
+    jpeg: 'file image',
+    pdf: 'file pdf',
+  };
+  return (
+    <div>
+      <i className={`${fileIcons[extension]} icon`}></i>
+      {name}
+    </div>
+  );
+}
 export default App;
